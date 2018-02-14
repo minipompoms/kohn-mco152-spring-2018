eb1a71e10aa652ce419a0c92ac3a3394a681743e
Paige Kohn <pkohn@student.touro.edu>
2018-02-14 09:41:12 -0500

    created dictionary test

---
+++ test/kohn/dictionary/DictionaryTest.java
@@ -0,0 +1,16 @@
+package kohn.dictionary;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DictionaryTest {
+	String fileName = "src/dictionary.txt";
+	Dictionary dictionary = new Dictionary(fileName);
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
