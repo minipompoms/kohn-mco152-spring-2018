8e216978645300b2320cebbd0d934f568067d3d4
Paige Kohn <pkohn@student.touro.edu>
2018-02-14 10:00:28 -0500

    updated tests

--- test/kohn/dictionary/DictionaryTest.java
+++ test/kohn/dictionary/DictionaryTest.java
@@ -9,8 +9,22 @@ public class DictionaryTest {
 	Dictionary dictionary = new Dictionary(fileName);
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDictionary() {
+		
+		assertNotNull(dictionary);		
+	}
+	
+	@Test
+	public void testContain() {
+		
+		String word = "CYCLIST";
+		assertTrue(word, dictionary.contains(word));
 	}
 
+	@Test
+	public void testDefinition()
+	{
+		String definition = " one who rides a bicycle [n -S]";
+		assertEquals(definition, dictionary.getDefinition("CYCLIST"));
+	}
 }
