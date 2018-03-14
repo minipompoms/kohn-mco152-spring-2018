fcc870fbde98c12abd93b61bdc600df527e21009
Paige Kohn <pkohn@student.touro.edu>
2018-03-14 08:24:22 -0400

    ProjectileGraph completed

--- src/kohn/physics/ProjectileGraph.java
+++ src/kohn/physics/ProjectileGraph.java
@@ -1,110 +1,81 @@
 package kohn.physics;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Point;
-import java.awt.event.ActionEvent;
-import java.util.ArrayList;
-
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JComponent;
+
 
 @SuppressWarnings("serial")
 public class ProjectileGraph extends JComponent {
 	
+	
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		Projectile projectile = new Projectile(42, 99);					
 		Font subScript = new Font("Courier", Font.PLAIN, 9);
 		g.setFont(subScript);
+		g.translate(60, 500);
 		
-		g.translate(50, 300);
-		for(int ix = 0; ix < 10; ++ix) {
-			projectile.setTime(ix);
-			int x = (int)projectile.getX();
-			int y = (int)projectile.getY();
-			projectile.setTime(ix+1);
-			int xx = (int)projectile.getX();
-			int yy = (int)projectile.getY();
-			g.setColor(Color.lightGray);
-			g.drawLine(x+2, -600, x+2, 0);
-			g.drawString("("+x+", "+y+")", x, -y); 
-			g.setColor(Color.RED);
-			g.fillOval(x, -y, 10, 10);
-			if(ix < 9) {
-				g.setColor(Color.GRAY);
-				g.drawLine(x, -y, xx, -yy );
+		int[]a = {36, 49, 42};
+		int [] v = {77, 109, 86};				
+		int x, y;
+		int count= 0;
+		BufferedImage ball = null;		
+		try {
+			 ball = ImageIO.read(new File("src/Tennis_Ball.png"));						
+		}catch(IOException e2) {
+			e2.printStackTrace();
+		}
+        Graphics2D g2 = (Graphics2D) g;
+		g2.setColor(Color.GREEN);
+	    BasicStroke wideStroke = new BasicStroke(1.5f);
+		g2.setStroke(wideStroke);
+
+		g2.draw3DRect(-30, -500, 750, 550, false);
+		
+		while(count <3) {
+			Projectile projectile = new Projectile(a[count], v[count]);	
+			
+			for(int ix = 0; ix < 10; ++ix) {
+				
+				projectile.setTime(ix);
+				 x = (int)projectile.getX();
+				 y = (int)projectile.getY();
+				 if(projectile.getAngle()== a[1]) {
+						g.setColor(Color.lightGray);				
+						g.drawLine(x+2, -700, x+2, 50);
+					} 
+				
+				g.drawImage(ball, x,-y, null);
+				projectile.setTime(ix+1);
+				int xx = (int)projectile.getX();
+				int yy = (int)projectile.getY();
+				g.setColor(Color.lightGray);				
+				g.drawString("("+x+", "+y+")", x, -y); 
+				
+				if(ix < 9) {
+					g.setColor(Color.GRAY);
+					g.drawLine(x, -y, xx, -yy );
+				}
 			}
-		}
-	
-		g.translate(50, 300);
-		
-		projectile.setAngle(40);
-		projectile.setVelocity(88);						
-		for(int ix = 0; ix < 5; ix++) {
 			
-			projectile.setTime(ix-.5);
-			int x = (int)projectile.getX();
-			int y = (int)projectile.getY();
-			
-			projectile.setTime(ix+.5);
-			int xx = (int)projectile.getX();
-			int yy = (int)projectile.getY();
-			g.setColor(Color.black);
-			g.drawString("("+x+", "+y+")", x, -y);
-			g.setColor(Color.green);
-			g.fillOval(x, -y, 7, 7);
-			g.setColor(Color.lightGray);
-			g.drawLine(x, -y, xx, -yy );
-			g.setColor(Color.GRAY);
-			g.drawLine(x+2, -200, x+2, 50);
-			 
-
-			projectile.setTime(ix);
-			x = (int)projectile.getX();
-			y = (int)projectile.getY();
-			projectile.setTime(ix+.5);
-			xx = (int)projectile.getX();
-			yy = (int)projectile.getY();
-			g.setColor(Color.black);		
-			g.drawString("("+x+", "+y+")", x, -y); 
-			g.setColor(Color.green);
-			g.fillOval(x, -y, 7, 7);
-			g.setColor(Color.LIGHT_GRAY);
-			g.drawLine(x, -y, xx, -yy );
-			g.setColor(Color.GRAY);
-			g.drawLine(x+2, -200, x+2, 50);	
+			count++;
 			
 		}
 		
-		g.translate(170, -30);
-		g.setColor(Color.LIGHT_GRAY);
-		g.fillRect(230, -250, 580, 495);
-		projectile.setAngle(44);
-		projectile.setVelocity(75);
-		for(int ix = 5; ix < 15; ++ix) {
-			projectile.setTime(ix+1);
-			int xx = (int)projectile.getX();
-			int yy = (int)projectile.getY();
-			projectile.setTime(ix);
-			int x = (int)projectile.getX();
-			int y = (int)projectile.getY();
-			g.setColor(Color.black);
-			g.fillOval(x, -y, 8, 8);
-			if (ix < 14) {
-				g.setColor(Color.RED);
-				g.drawLine(x, -y, xx, -yy);
-			}			
-			g.setColor(Color.DARK_GRAY);
-			g.drawLine(x+2, -250, x+2, 243);	
-			g.drawString("("+x+", "+y+")", x, -y); 
-
-		}
-		
-	
 		 }
 	
+	
 }
+
--- src/kohn/physics/ProjectileGraphGui.java
+++ src/kohn/physics/ProjectileGraphGui.java
@@ -1,7 +1,13 @@
 package kohn.physics;
 
 import java.awt.BorderLayout;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -9,18 +15,17 @@ import javax.swing.JPanel;
 public class ProjectileGraphGui extends JFrame{
 
 	public ProjectileGraphGui(){
-		//hw 2: draw the coordinates(string)
 		
-		//hw 5: projectile image
 		
 		setTitle("Projectile Viewer");
-		setSize(1350, 1000);
+		setSize(800, 615);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
+		setLocation(340,90);
 		JPanel panel = new JPanel();
 		panel.setLayout(new BorderLayout());
 		
 		panel.add(new ProjectileGraph(), BorderLayout.CENTER);
+		
 		
 		setContentPane(panel);
 	}
