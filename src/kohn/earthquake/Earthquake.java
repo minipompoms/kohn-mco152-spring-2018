cc86cb1903b804fc9de753d14ecd2774360aaf6a
Paige Kohn <pkohn@student.touro.edu>
2018-05-10 18:15:53 -0400

    update with master

--- src/kohn/earthquake/Earthquake.java
+++ src/kohn/earthquake/Earthquake.java
@@ -5,6 +5,7 @@ public class Earthquake {
 
 	private String id;
 	private EarthquakeProperties properties;
+	
 	public Earthquake(double mag, String place, long time) {
 		properties = new EarthquakeProperties(mag,place,time);
 	}
